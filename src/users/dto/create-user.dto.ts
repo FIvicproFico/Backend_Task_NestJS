1529ea15c8d72949300bff40648325de36b792aa
FIvicproFico <fivic@profico.hr>
2022-03-08 13:58:37 +0100

    Added class validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @Length(0, 20)
   username: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   surname: string;
+
+  @IsEmail()
   email: string;
+
   role: string;
 }
